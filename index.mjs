be74ef26555837c1009ba93895253b13a49d0ede
Girma <93812274+206724@users.noreply.github.com>
2024-05-23 01:41:57 -0700

    my second commit

--- index.mjs
+++ index.mjs
@@ -28,6 +28,61 @@ menu.forEach(menulinks =>{
 
 })
 
+dynamicUI()
+function dynamicUI(){
+const divElemnet =document.createElement('div');
+divElemnet.setAttribute('id','divElementID');
+divElemnet.classList.add('div-container');
+
+const paraElement =document.createElement('p');
+paraElement.innerHTML-'Login';
+paraElement.style.display= 'flex';
+paraElement.style.justifyContent = 'center';
+
+const inputElement = document.createElement('input');
+inputElement.placeholder ='Enter Usernname';
+inputElement.id = 'Username_id';
+inputElement.type = 'text';
+inputElement.classList.add('username-container')
+
+const passwordElement = document.createElement('input');
+passwordElement.placeholder = 'Enter password';
+passwordElement.id='password_id';
+passwordElement.type='password';
+passwordElement.classList.add('username-container')
+
+const btnContainer = document.createElement('div')
+btnContainer.style.display='flex';
+btnContainer.style.justifyContent='end';
+
+const btnElement = document.createElement('button')
+btnElement.innerHTML = 'Login';
+btnElement.id ='btn_id';
+btnElement.type = 'button';
+
+btnContainer.appendChild(btnElement);
+
+divElemnet.appendChild(paraElement);
+divElemnet.appendChild(inputElement);
+divElemnet.appendChild(passwordElement);
+divElemnet.appendChild(btnContainer);
+
+document.body.appendChild(divElemnet);
+
+}//Use the parent-child-sibling relationship to navigate between elements at
+// least once (firstChild, lastChild, parentNode, nextElementSibling, etc.).
+
+const discountSeason = document.querySelector("#Discount > li:first-of-type")
+// console.log(discountSeason);
+const lastMinBooking=discountSeason.nextElementSibling;
+
+const tourstLocation=discountSeason.nextSibling;
+
+// const paragraphs = document.getElementsByTagName('p')
+
+
+
+
 
 
 
